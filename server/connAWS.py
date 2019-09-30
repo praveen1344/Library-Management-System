864ebbb62c6d8b8bbbe48672a6f5964671c741a6
Drewqilong <drewqilong@live.com>
2019-09-30 12:16:35 -0500

    Connect AWS

---
+++ server/connAWS.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 26 13:05:16 2019
+
+@author: Jerry
+"""
+from flask import Flask
+from sshtunnel import SSHTunnelForwarder
+from flask_sqlalchemy import SQLAlchemy
+
+def dbConn():
+    # ssh variables
+    host = '34.209.47.103'
+    localhost = '127.0.0.1'
+    ssh_username = 'root'
+#    ssh_password = '123456'
+    ss_public_key = 'Clusterkey.pem'
+    # database variables
+    user='root'
+    password='123456'
+    database='mysql'
+    app = Flask(__name__)
+    server =  SSHTunnelForwarder(
+         (host, 22),
+#         ssh_password= ssh_password,
+         ssh_pkey = ss_public_key,
+         ssh_username = ssh_username,
+         remote_bind_address=(localhost, 3306))
+    
+    server.start()
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123456@127.0.0.1:{}/mysql'.format(server.local_bind_port)
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://127.0.0.1'
+    db = SQLAlchemy(app)
+    return db
+    
+#server.stop()
